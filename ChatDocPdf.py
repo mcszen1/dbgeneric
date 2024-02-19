727d06176aacff680c2bf4540d8e815321a66c88
mcszen1 <mcszen@gmail.com>
2024-02-19 17:06:19 -0300

    Update ChatDocPdf.py

--- ChatDocPdf.py
+++ ChatDocPdf.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from io import BytesIO
-from google.colab import drive
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -11,7 +10,7 @@ from langchain.chains import ConversationalRetrievalChain
 import os
 
 # Constants and API Keys
-OPENAI_API_KEY = "your_openai_api_key"  # Replace with your actual API key
+#OPENAI_API_KEY = "your_openai_api_key"  # Replace with your actual API key
 GPT_MODEL_NAME = 'gpt-4'
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 50
